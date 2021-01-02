86c141b4569d07fb4bef0532aaf43cb6fa353143
iNoursh <nour@hotmail.it>
2021-01-02 14:02:15 +0300

    working on it

--- app/src/main/java/com/inour/homeneeds/MainActivity.kt
+++ app/src/main/java/com/inour/homeneeds/MainActivity.kt
@@ -2,10 +2,40 @@ package com.inour.homeneeds
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.inour.homeneeds.fragments.AddNewItemFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AddNewItemFragment.OnFragmentInteractionListener {
+    var addNewItem: FrameLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addNewItem = findViewById(R.id.addNewItem)
     }
+
+    override fun onFragmentInteractionListener(type: String) {
+        if (type == "cancel") {
+            hideAddNewItem()
+        }
+    }
+
+    fun hideAddNewItem() {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        val fragment = supportFragmentManager.fragments[0]
+        transaction.setCustomAnimations(R.anim.fragment_fade_enter, R.anim.fragment_fade_exit)
+        transaction.remove(fragment).commit()
+    }
+
+    fun showAddNewItem(view: View) {
+        val fragment: AddNewItemFragment = AddNewItemFragment.newInstance()
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(R.anim.fragment_fade_enter, R.anim.fragment_fade_exit)
+        transaction.add(R.id.addNewItem, fragment, "Add_New_Item").commit()
+    }
+
+
 }
---
+++ app/src/main/java/com/inour/homeneeds/fragments/AddNewItemFragment.kt
@@ -0,0 +1,63 @@
+package com.inour.homeneeds.fragments
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.NumberPicker
+import com.inour.homeneeds.R
+
+class AddNewItemFragment : Fragment() {
+    var numberPicker: NumberPicker? = null
+    private var mListener: OnFragmentInteractionListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            mListener = context
+        } else {
+            throw RuntimeException(requireContext().toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+    override fun onDetach() {
+        super.onDetach()
+        mListener = null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_new_item, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        numberPicker = view.findViewById(R.id.editCount)
+        numberPicker?.maxValue = 10
+        numberPicker?.minValue = 0
+        val cancelBtn: Button = view.findViewById(R.id.btnCancel)
+        cancelBtn.setOnClickListener {
+            hideAddNewItem()
+        }
+    }
+
+    private fun hideAddNewItem() {
+        if(mListener != null) {
+            mListener?.onFragmentInteractionListener("cancel")
+        }
+    }
+
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteractionListener(type: String)
+    }
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            AddNewItemFragment().apply {}
+    }
+}
